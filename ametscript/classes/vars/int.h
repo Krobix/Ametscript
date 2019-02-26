a07a3b23a859d79891392ef18911dec062bfbab2
Krobix <45689440+Krobix@users.noreply.github.com>
2019-02-26 07:56:39 -0500

    Update int.h

--- ametscript/classes/vars/int.h
+++ ametscript/classes/vars/int.h
@@ -2,18 +2,23 @@
 
 class aint{
 private: 
-int value;
-int maxval;
-int minval;
-bool valrestricted;
+  int value;
+  int maxval;
+  int minval;
+  bool valrestricted;
 
 public:
-aint(int value, int minval, int maxval);
-aint(int value);
-aint();
-int getValue();
-void changeValue();
-void changeMax();
-void changeMin();
+  aint(int value, int minval, int maxval);
+  aint(int value);
+  aint();
+  int getValue();
+  void changeValue();
+  void changeMax();
+  void changeMin();
 
 };
+
+class restrictedInt:public aint{
+public:
+  restrictedInt(int value);
+}
