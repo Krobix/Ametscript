27e292809c80a4b83ad86e879e5c776743ed8693
Krobix <45689440+Krobix@users.noreply.github.com>
2019-02-26 11:31:14 -0500

    Update int.cpp

--- ametscript/classes/vars/int.cpp
+++ ametscript/classes/vars/int.cpp
@@ -1,1 +1,19 @@
 #include "int.h"
+#include "../error.h"
+
+
+aint::aint(int value, int minval, int maxval){
+  this->value = value;
+  this->minval = minval;
+  this->maxval = maxval;
+}
+aint::aint(int value){
+  this->value = value;
+  this->maxval = 345259;
+  this->minval = -345259;
+}
+aint::aint(){
+  this->value = 0;
+  this->maxval = 345529;
+  this->minval = -345529;
+}
