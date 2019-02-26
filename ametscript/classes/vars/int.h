282aaebbd55002a6390ec1f153a3a818054a27a7
Krobix <45689440+Krobix@users.noreply.github.com>
2019-02-26 07:47:14 -0500

    Update and rename ametscript/classes/vars.h to ametscript/classes/vars/int.h

---
+++ ametscript/classes/vars/int.h
@@ -0,0 +1,19 @@
+
+
+class aint{
+private: 
+int value;
+int maxval;
+int minval;
+bool valrestricted;
+
+public:
+aint(int value, int minval, int maxval);
+aint(int value);
+aint();
+int getValue();
+void changeValue();
+void changeMax();
+void changeMin();
+
+};
