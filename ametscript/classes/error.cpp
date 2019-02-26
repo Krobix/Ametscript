db71049586882c7d90deda69b42b945c3be1560d
Krobix <45689440+Krobix@users.noreply.github.com>
2019-02-26 13:36:19 -0500

    Update error.cpp

--- ametscript/classes/error.cpp
+++ ametscript/classes/error.cpp
@@ -3,8 +3,9 @@
 error::error(std::string id){
   this->id = id;
 }
-void error::raise(){
-  
+void error::raise(std::string callback, std::string msg){
+  std::cout << this->id << ":\n";
+  std::cout << callback << ", details:" << msg << "\n";
 }
 
 
