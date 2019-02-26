0aa6b0bec87c16eff28f026b35918e2f12a10c08
Krobix <45689440+Krobix@users.noreply.github.com>
2019-02-26 11:39:59 -0500

    Update error.h

--- ametscript/classes/error.h
+++ ametscript/classes/error.h
@@ -3,7 +3,13 @@
 
 
 class error{
-
+private:
+  std::string id;
+  std::string msg;
+  std::string raisedBy;
+public:
+  error(std::string id);
+  void raise(std::string callback, std::string msg); 
 };
 
 
