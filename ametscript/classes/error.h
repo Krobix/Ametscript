d31c43dea4aad8afe8cbc833622354da135fd6ac
Krobix <45689440+Krobix@users.noreply.github.com>
2019-02-27 11:15:41 -0500

    Update error.h

--- ametscript/classes/error.h
+++ ametscript/classes/error.h
@@ -1,10 +1,9 @@
 #ifndef ERROR_H
 #define ERROR_H
+#include "vars/object.h"
 
 
-class error{
-private:
-  std::string id;
+class error:public object{
 public:
   error(std::string id);
   void raise(std::string callback, std::string msg); 
