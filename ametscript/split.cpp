e9c8d05027310b2e1e989acb24db384c7a0530ed
Krobix <45689440+Krobix@users.noreply.github.com>
2019-02-27 12:28:41 -0500

    Update split.cpp

--- ametscript/split.cpp
+++ ametscript/split.cpp
@@ -1,4 +1,8 @@
 #include "split.h"
 
-std::vector<std::string> split(std::string str, std::string splitToken{
+std::vector<std::string> split(std::string str, std::string splitToken){
+  std::vector<std::string> splitVect;
+  if(str.find
+  
+  
 }
