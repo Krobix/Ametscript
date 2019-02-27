f940ce5f10d44e5c3830dbb7bd9e9acf1f5105f4
Krobix <45689440+Krobix@users.noreply.github.com>
2019-02-27 08:13:40 -0500

    Update object.cpp

--- ametscript/classes/vars/object.cpp
+++ ametscript/classes/vars/object.cpp
@@ -1,3 +1,10 @@
 #include "object.h"
 
-object::object(){}
+object::object(std::string name, bool isConst){
+  this->name = name;
+  this->isConst = isConst;
+}
+object::object(){
+  this->name = "__obj__";
+  this->isConst = false;
+}
