2179bda16d6523b053fcf75990a29862bb965d3c
Krobix <45689440+Krobix@users.noreply.github.com>
2019-02-28 18:47:20 -0500

    Create lexer.h

---
+++ ametscript/lexer.h
@@ -0,0 +1,9 @@
+#ifndef LEXER_H
+#define LEXER_H
+
+std::string getLoopContents(std::string fullScript);
+int evaluate(std::string expression);
+std::string checkStatementType(std::string statement);
+
+
+#endif
