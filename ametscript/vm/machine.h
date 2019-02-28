674877f6094c118f7635c8ad18ef9e74e6b602e8
Krobix <45689440+Krobix@users.noreply.github.com>
2019-02-27 21:00:23 -0500

    Update machine.h

--- ametscript/vm/machine.h
+++ ametscript/vm/machine.h
@@ -4,19 +4,21 @@
 
 
 enum ops{
-    LOAD_MAIN, //0x00
-    EXIT_MAIN, //0x01
+    LOAD_CORE, //0x00
+    EXIT_CORE, //0x01
     NEW_OBJ,   //0x02
     GET_VAR,   //0x03
     SET_VAR,   //0x04
     DEF_FUNC,  //0x05
-    CALL_FUNC,
-    IMPORT_MOD,
-    CREATE_MOD,
-    BIN_ADD,
-    BIN_SUBTRACT,
-    BIN_MULT,
-    BIN_DIV
+    CALL_FUNC, //0x06
+    IMPORT_MOD,//0x07
+    CREATE_MOD,//0x08
+    
+    BIN_ADD,   //0xAA
+    BIN_SUBTRACT,//0xAB
+    BIN_MULT,//0xAC
+    BIN_DIV,//0xAD
+    
     
  };
 
