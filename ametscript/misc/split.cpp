b6e04b3c7847eb7e12f76e938f519400da508bdb
Krobix <45689440+Krobix@users.noreply.github.com>
2019-02-28 16:17:24 -0500

    Update split.cpp

--- ametscript/misc/split.cpp
+++ ametscript/misc/split.cpp
@@ -1,10 +1,15 @@
 #include "split.h"
+#include <bits/stdc++.h> 
 
 std::vector<std::string> split(std::string str, std::string splitToken){
   std::vector<std::string> splitVect;
   int tempPos;
+  char totalStringArray[str.length + 1];
+  strcopy(totalStringArray, str.c_str());
   while(!(str.find(splitToken) = std::string::npos)){
     tempPos = str.find(splitToken);
+    
+    
   }
   
   
