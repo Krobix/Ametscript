d803ac53936c5c95546f21b4c8ff920565532c2d
Krobix <45689440+Krobix@users.noreply.github.com>
2019-02-28 08:11:04 -0500

    Update machine.h

--- ametscript/vm/machine.h
+++ ametscript/vm/machine.h
@@ -1,6 +1,7 @@
 #ifndef MACHINE_H
 #define MACHINE_H
 #include "opcode.h"
+#include "../classes/vars/object.h"
 
 
 enum ops{
@@ -14,13 +15,22 @@ enum ops{
     IMPORT_MOD,//0x07
     CREATE_MOD,//0x08
     
+    //Mathematical Operations
     BIN_ADD = 160,   //0xA0
     BIN_SUBTRACT = 161,//0xA1
     BIN_MULT = 162,//0xA2
     BIN_DIV = 163,//0xA3
+    CMP_EQUAL = 164,//0xA4
+    CMP_LESS = 165,//0xA5
+    CMP_GREAT = 166//0xA6
     
     
  };
+
+class machine{
+private:
+     
+};
 
 
 #endif
