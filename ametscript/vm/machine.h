cd1cd258db3b6cdab68d989e799dba6ea4f4e0d5
Krobix <45689440+Krobix@users.noreply.github.com>
2019-02-27 20:10:40 -0500

    added comments to show which is which byte

--- ametscript/vm/machine.h
+++ ametscript/vm/machine.h
@@ -4,14 +4,19 @@
 class machine{
 public:
   enum ops{
-    LOAD_MAIN,
-    EXIT_MAIN,
-    NEW_OBJ,
-    GET_VAR,
-    SET_VAR,
-    DEF_FUNC,
+    LOAD_MAIN, //0x00
+    EXIT_MAIN, //0x01
+    NEW_OBJ,   //0x02
+    GET_VAR,   //0x03
+    SET_VAR,   //0x04
+    DEF_FUNC,  //0x05
     CALL_FUNC,
-    IMPORT_MOD
+    IMPORT_MOD,
+    CREATE_MOD,
+    BIN_ADD,
+    BIN_SUBTRACT,
+    BIN_MULT,
+    BIN_DIV
     
   };
 };
