4b54428986c52fddf648cf86f798f5ab2ee6d7be
Krobix <45689440+Krobix@users.noreply.github.com>
2019-02-27 19:06:49 -0500

    Update machine.h

--- ametscript/vm/machine.h
+++ ametscript/vm/machine.h
@@ -1,6 +1,19 @@
 #ifndef MACHINE_H
 #define MACHINE_H
 
-
+class machine{
+public:
+  enum ops{
+    LOAD_MAIN,
+    EXIT_MAIN,
+    NEW_OBJ,
+    GET_VAR,
+    SET_VAR,
+    DEF_FUNC,
+    CALL_FUNC,
+    IMPORT_MOD
+    
+  }
+};
 
 #endif
