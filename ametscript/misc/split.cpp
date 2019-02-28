23780398e740dae75682c3993e7c5abafc963b82
Krobix <45689440+Krobix@users.noreply.github.com>
2019-02-28 16:08:22 -0500

    Update split.cpp

--- ametscript/misc/split.cpp
+++ ametscript/misc/split.cpp
@@ -2,8 +2,9 @@
 
 std::vector<std::string> split(std::string str, std::string splitToken){
   std::vector<std::string> splitVect;
+  int tempPos;
   while(!(str.find(splitToken) = std::string::npos)){
-    int tempPos = str.find(splitToken);
+    tempPos = str.find(splitToken);
   }
   
   
