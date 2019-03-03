031c5cdb287b7ab78ac9482b427a1a867c8d206a
Krobix <45689440+Krobix@users.noreply.github.com>
2019-03-02 21:00:51 -0500

    Update string.h

--- ametscript/classes/vars/string.h
+++ ametscript/classes/vars/string.h
@@ -1,8 +1,14 @@
+#ifndef STRING_H
+#define STRING_H
 #include "object.h"
 
 class astring:public object{
 protected:
   std::string value;
+  int length;
 public:
   astring(std::string v);
 };
+
+
+#endif
