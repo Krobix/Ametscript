022456c6b771e107c79dc3557e976bcdad444952
Krobix <45689440+Krobix@users.noreply.github.com>
2019-03-12 11:00:31 -0400

    Delete lexer.h

--- ametscript/lexer.h
+++
@@ -1,9 +0,0 @@
-#ifndef LEXER_H
-#define LEXER_H
-
-std::string getLoopContents(std::string fullScript);
-int evaluate(std::string expression);
-std::string checkStatementType(std::string statement);
-
-
-#endif
